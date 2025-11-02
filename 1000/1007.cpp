d6c9c3cdc87c5982af15e536905dd91365254bb2
Typezera <willianbardela2131@hotmail.com>
2025-11-02 17:35:16 -0300

    desafio 1007 concluido

---
+++ 1000/1007.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    int a, b, c, d;
+
+    std::cin >> a >> b >> c >> d;
+
+    int result = (a * b) - (c * d);
+
+    std::cout << "DIFERENCA = " << result << std::endl;
+
+    return 0;
+}
