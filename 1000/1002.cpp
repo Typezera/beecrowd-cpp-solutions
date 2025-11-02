33fa56459ea01313bb1349b1b1ca0c1ec5fb9c1a
Typezera <willianbardela2131@hotmail.com>
2025-11-02 16:22:45 -0300

    desafio 1002 concluido

---
+++ 1000/1002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int a; 
+    int b;
+
+    std::cin >> a;
+    std::cin >> b;
+    int resultado = a + b;
+
+    std::cout << "X = " << resultado << std::endl;
+
+    return 0;
+}
