d024ebfc60426209fe1ed996f1664ac524521c4e
Typezera <willianbardela2131@hotmail.com>
2025-11-02 16:52:53 -0300

    desafio 1004 concluido

---
+++ 1000/1004.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main () {
+
+    int a, b, prod;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    prod = a * b;
+
+    std::cout << "PROD = " << prod << std::endl;
+
+    return 0;
+}
